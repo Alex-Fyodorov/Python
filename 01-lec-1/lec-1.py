aecea5b8566a401a4e1bb1a56591d6ab6a47e16c
alexf <rassylka82@inbox.ru>
2023-07-12 21:09:02 +0300

    lec-1

---
+++ 01-lec-1/lec-1.py
@@ -0,0 +1,12 @@
+# a = int(input('Vvedite pervoe chislo: '))
+# b = int(input('Vvedite vtoroe chislo: '))
+# print(f"summa: {a + b}")
+
+# a = 3.165465
+# b = 8.89965465
+# print(a * b)
+# print(round(a * b, 3))
+
+# a = 'qwerty'
+# for i in a:
+#     print(i)
